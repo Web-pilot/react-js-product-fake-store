4b618a7d343678564e76021c2723ca049a51fc20
ochagwu paul <ochagwupaul295@gmail.com>
2022-10-01 19:08:05 +0100

    edited smallcard components

--- src/components/SmallCard.js
+++ src/components/SmallCard.js
@@ -3,20 +3,17 @@ import { Link } from "react-router-dom";
 
 const SmallCard = ({ product }) => {
   return (
-    <div className="flex_box_container">
-      <a href="">
+    <Link to={`/product/${product.id}`}>
+      <div className="flex_box_container">
         <img src={product.image} alt="" />
-      </a>
-      <ul className="latest_recipe_list">
-        <li>
-          <h3>
-            <Link to={`/product/${product.id}`}>
-              {product.title.slice(0, 10)}... 
-            </Link>
-          </h3>
-        </li>
-      </ul>
-    </div>
+
+        <ul className="latest_recipe_list">
+          <li>
+            <h3>{product.title.slice(0, 10)}...</h3>
+          </li>
+        </ul>
+      </div>
+    </Link>
   );
 };
 
