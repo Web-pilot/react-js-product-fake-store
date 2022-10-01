9ab91c8cfa0a8ad1708a24b9702f2271ba716508
ochagwu paul <ochagwupaul295@gmail.com>
2022-10-01 19:42:42 +0100

    css update

--- src/components/SmallCard.js
+++ src/components/SmallCard.js
@@ -6,7 +6,6 @@ const SmallCard = ({ product }) => {
     <Link to={`/product/${product.id}`}>
       <div className="flex_box_container">
         <img src={product.image} alt="" />
-
         <ul className="latest_recipe_list">
           <li>
             <h3>{product.title.slice(0, 10)}...</h3>
